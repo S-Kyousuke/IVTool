108469d687f5b0d48a5370ec28792d2973724082
S. Kyousuke <surasek@gmail.com>
2016-09-14 11:34:51 +0700

    Add copyright, release 1.0.0

--- app/src/main/java/com/skyousuke/ivtool/windows/ResultWindow.java
+++ app/src/main/java/com/skyousuke/ivtool/windows/ResultWindow.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 Surasek Nusati <surasek@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.skyousuke.ivtool.windows;
 
 import android.annotation.SuppressLint;
@@ -17,12 +33,12 @@ import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import com.skyousuke.ivtool.view.BackKeyListener;
 import com.skyousuke.ivtool.IVResult;
-import com.skyousuke.ivtool.view.IVResultAdapter;
 import com.skyousuke.ivtool.Pokemon;
 import com.skyousuke.ivtool.R;
+import com.skyousuke.ivtool.view.BackKeyListener;
 import com.skyousuke.ivtool.view.CustomRelativeLayout;
+import com.skyousuke.ivtool.view.IVResultAdapter;
 
 import java.util.List;
 
